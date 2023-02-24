378aa83ffa7198e5c722b0ee43dc01e79805b7ce
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2023-02-24 17:28:37 +0100

    better control

--- plot_graph.py
+++ plot_graph.py
@@ -11,22 +11,29 @@ parser = argparse.ArgumentParser(description='CursusDraw')
 parser.add_argument('input_filename', type=str,
                     help="Json file with cursus information")
 parser.add_argument('--infos', type=str,
-                    default='ID,Degree,Year,ECTS,Semester',
+                    default='',
                     help="Json file with cursus information")
-parser.add_argument('--output', '-o', type=str, default='test.pdf',
-                    help="Json file with cursus information")
-
+parser.add_argument('--output', '-o', type=str, default='',
+                    help="File to produce")
+parser.add_argument('--show_prereq', action='store_true',
+                    help="Requests to show pre-requisites as edges")
 
 arguments = parser.parse_args()
 filename = arguments.input_filename
 output = arguments.output
+if output == '':
+    input_basename, ext = os.path.splitext(filename)
+    output = input_basename + '.pdf'
+    output = output.replace(' ', '_')
 output_basename, ext = os.path.splitext(output)
+infos = arguments.infos.split(',')
+infos = [e for e in infos if e.strip() != '']
 print(output_basename)
 ################################################################
 
 txt = open(filename).read()
 content = json.loads(txt)
-dot = create_graph(content)
+dot = create_graph(content, infos, show_prereq=arguments.show_prereq)
 
 dot.save(f'{output_basename}.dot')
 
