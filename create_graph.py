cf0406e191df744ce5c763e4b6d495e28ab929ac
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2023-02-24 16:14:21 +0100

    splitting done

--- create_graph.py
+++ create_graph.py
@@ -4,56 +4,55 @@ import graphviz
 ################################################################
 
 
-def create_year_graph(year):
-
-    graph = graphviz.Digraph(
-        name=f'cluster_year{year}',
-        body=["graph[style=bold];"])
+def create_year_graph(title):
+    return graphviz.Digraph(name=title, body=["graph[style=bold];"])
 
 
-infos = ['ID', 'Degree', 'Year', 'ECTS', "Semester"]
+def create_graph(content, infos=['ID', 'Degree', 'Year', 'ECTS', "Semester"]):
+    years = [create_year_graph('cluster_year1'), create_year_graph(
+        'cluster_year2'), create_year_graph('cluster_year3')]
 
-courses = set()
+    courses = set()
 
-for i, e in enumerate(content):
-    title = e['Course Title']
-    if title in courses:
-        raise RuntimeError(f'class {title} already registered')
-    courses.add(title)
-    year = e['Year']
-    label = '{' + title + '|'
-    for _i in infos:
-        if _i in e:
-            label += '\\n' + str(_i) + ': ' + str(e[_i])
-    label += '}'
+    for i, e in enumerate(content):
+        title = e['Course Title']
+        if title in courses:
+            raise RuntimeError(f'class {title} already registered')
+        courses.add(title)
+        year = e['Year']
+        label = '{' + title + '|'
+        for _i in infos:
+            if _i in e:
+                label += '\\n' + str(_i) + ': ' + str(e[_i])
+        label += '}'
 
-    dot = years[year-1]
-    dot.node(title, label=label, URL=e['URL'])
-    prereq = e["Pre-requisites"]
-    if prereq:
-        for p in prereq:
-            print(title, p)
-            dot.edge(p, title, style='dotted')
+        dot = years[year-1]
+        dot.node(title, label=label, URL=e['URL'])
+        prereq = e["Pre-requisites"]
+        if prereq:
+            for p in prereq:
+                print(title, p)
+                dot.edge(p, title, style='dotted')
 
-# check pre-req dependencies
-for i, e in enumerate(content):
-    prereq = e["Pre-requisites"]
-    if prereq:
-        for p in prereq:
-            if p not in courses:
-                print(f'pre-req class {p} not found in courses')
+    # check pre-req dependencies
+    for i, e in enumerate(content):
+        prereq = e["Pre-requisites"]
+        if prereq:
+            for p in prereq:
+                if p not in courses:
+                    print(f'pre-req class {p} not found in courses')
 
-dot = graphviz.Digraph(
-    comment='Bachelor',
-    node_attr={'shape': 'record'},
-    body='''
-    rankdir=LR
-    edge[fontname="Helvetica", fontsize="10",
-    labelfontname="Helvetica", labelfontsize="10"]
-    node[fontname="Helvetica", fontsize="10", shape=record]
-    ''')
+    dot = graphviz.Digraph(
+        comment='Bachelor',
+        node_attr={'shape': 'record'},
+        body='''
+        rankdir=LR
+        edge[fontname="Helvetica", fontsize="10",
+        labelfontname="Helvetica", labelfontsize="10"]
+        node[fontname="Helvetica", fontsize="10", shape=record]
+        ''')
 
-for y in years:
-    dot.subgraph(y)
+    for y in years:
+        dot.subgraph(y)
 
-return dot
+    return dot
--- plotting.py
+++ plotting.py
@@ -1,76 +1,35 @@
 #!/bin/env python
 ################################################################
 import json
-import graphviz
+import os
 import subprocess
 import argparse
-import sys
+from create_graph import create_graph
 ################################################################
 
 parser = argparse.ArgumentParser(description='CursusDraw')
 parser.add_argument('input_filename', type=str,
                     help="Json file with cursus information")
+parser.add_argument('--infos', type=str,
+                    default='ID,Degree,Year,ECTS,Semester',
+                    help="Json file with cursus information")
+parser.add_argument('--output', '-o', type=str, default='test.pdf',
+                    help="Json file with cursus information")
+
 
 arguments = parser.parse_args()
 filename = arguments.input_filename
-
+output = arguments.output
+output_basename, ext = os.path.splitext(output)
+print(output_basename)
 ################################################################
 
 txt = open(filename).read()
 content = json.loads(txt)
+dot = create_graph(content)
 
+dot.save(f'{output_basename}.dot')
 
-def create_graph(title):
-    return graphviz.Digraph(name=title, body=["graph[style=bold];"])
-
-
-years = [create_graph('cluster_year1'), create_graph(
-    'cluster_year2'), create_graph('cluster_year3')]
-
-infos = ['ID', 'Degree', 'Year', 'ECTS', "Semester"]
-
-courses = set()
-
-for i, e in enumerate(content):
-    title = e['Course Title']
-    if title in courses:
-        raise RuntimeError(f'class {title} already registered')
-    courses.add(title)
-    year = e['Year']
-    label = '{' + title + '|'
-    for _i in infos:
-        if _i in e:
-            label += '\\n' + str(_i) + ': ' + str(e[_i])
-    label += '}'
-
-    dot = years[year-1]
-    dot.node(title, label=label, URL=e['URL'])
-    prereq = e["Pre-requisites"]
-    if prereq:
-        for p in prereq:
-            print(title, p)
-            dot.edge(p, title, style='dotted')
-
-# check pre-req dependencies
-for i, e in enumerate(content):
-    prereq = e["Pre-requisites"]
-    if prereq:
-        for p in prereq:
-            if p not in courses:
-                print(f'pre-req class {p} not found in courses')
-
-dot = graphviz.Digraph(
-    comment='Bachelor',
-    node_attr={'shape': 'record'},
-    body='''
-    rankdir=LR
-    edge[fontname="Helvetica", fontsize="10",
-    labelfontname="Helvetica", labelfontsize="10"]
-    node[fontname="Helvetica", fontsize="10", shape=record]
-    ''')
-
-for y in years:
-    dot.subgraph(y)
-
-dot.save('test.dot')
-subprocess.call('dot -Tpdf -o test.pdf test.dot', shell=True)
+cmd = f'dot -T{ext[1:]} -o{output} {output_basename}.dot'
+print(cmd)
+subprocess.call(cmd, shell=True)
