836946ec82d962c9ce0043c0c7dbccffc136f7a8
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2023-08-31 09:26:14 +0200

    cleaning

--- plot_treemap.py
+++ plot_treemap.py
@@ -4,12 +4,12 @@ import streamlit as st
 import plot_treemap_bachelor
 import plot_treemap_master
 import plot_all_match
-import plot_statistics
+import plot_best_match
 
 st.set_page_config(layout="wide")
 
-tab_explore, tab_match, tab_statistics = st.tabs(
-    ["Explore", "Matching Classes", "Statistics"])
+tab_explore, tab_match, tab_best_match, tab_per_class = st.tabs(
+    ["Explore", "Matching Classes", "Best Matches", "Per Class Match"])
 
 with tab_explore:
     opt = st.radio('Choose Degree', options=['Bachelor', 'Master'])
@@ -23,5 +23,5 @@ with tab_explore:
 with tab_match:
     plot_all_match.main()
 
-with tab_statistics:
-    plot_statistics.main()
+with tab_best_match:
+    plot_best_match.main()
