87e5fd3e3dd7b92f90758fa09e8d3b3fd8786c4d
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2023-08-31 11:31:27 +0200

    saving

--- plot_search.py
+++ plot_search.py
@@ -54,30 +54,22 @@ def _main(params):
                   for e in _filenames if (not e.startswith('Master_')
                                           and not e.startswith('match_'))]
 
-    _filenames.remove('EPFL')
-    courses_matches = {}
-    for uni in _filenames:
-        try:
-            with open(f'match_{uni}.json', "r") as f:
-                match = yaml.safe_load(f.read())
-                df = pd.read_json(uni+'.json')
-                courses_matches[uni] = {}
-                for k, v in match.items():
-                    courses_matches[uni][k] = df[df['Course Title']
-                                                 == v].iloc[0]
-        except Exception:
-            st.error(f"Matching tab should be executed once at least {uni}")
-            return
-
-    df_epfl = pd.read_json('EPFL.json')
-    options = df_epfl.sort_values(by=['Year', 'Semester', 'Course Title'])[
-        'Course Title']
-
-    def _fmt(opt):
-        lab = df_epfl[df_epfl['Course Title'] == opt].iloc[0]
-        return (f"{lab['Course Title']}"
-                f" (Year {lab['Year']} - {lab['Semester']} - {lab['ECTS']} ECTS)")
-
+    with st.spinner('Loading'):
+        if 'courses' not in st.session_state:
+            courses = {}
+            for uni in _filenames:
+                try:
+                    df = pd.read_json(uni+'.json')
+                    courses[uni] = df
+                except Exception:
+                    st.error(
+                        f"Matching tab should be executed once at least {uni}")
+                    return
+            st.session_state['courses'] = courses
+    courses = st.session_state['courses']
+    search = st.text_input('Search in all registered classes')
+    st.write(search)
+    return
     idx = 0
     if 'class' in params:
         c = params['class'][0]
@@ -95,6 +87,5 @@ def _main(params):
         st.markdown('---')
 
 
-def main(params):
-    with st.spinner('Loading'):
-        _main(params)
+def main(params={}):
+    _main(params)
--- plot_treemap.py
+++ plot_treemap.py
@@ -6,26 +6,38 @@ import plot_treemap_master
 import plot_all_match
 import plot_best_match
 import plot_per_class_match
+import plot_search
 
 st.set_page_config(layout="wide")
+params = st.experimental_get_query_params()
 
-tab_explore, tab_match, tab_best_match, tab_per_class = st.tabs(
-    ["Explore", "Matching Classes", "Best Matches", "Per Class Match"])
+if 'view' in params and params['view'][0] == 'tab_per_class':
+    plot_per_class_match.main(params)
+elif 'view' in params and params['view'][0] == 'tab_search':
+    plot_search.main(params)
 
-with tab_explore:
-    opt = st.radio('Choose Degree', options=['Bachelor', 'Master'])
+else:
+    tab_explore, tab_match, tab_best_match, tab_per_class, tab_search = st.tabs(
+        ["Explore", "Matching Classes",
+         "Best Matches", "Per Class Match", "search"])
 
-    if opt == 'Bachelor':
-        plot_treemap_bachelor.main()
+    with tab_explore:
+        opt = st.radio('Choose Degree', options=['Bachelor', 'Master'])
 
-    if opt == 'Master':
-        plot_treemap_master.main()
+        if opt == 'Bachelor':
+            plot_treemap_bachelor.main()
 
-with tab_match:
-    plot_all_match.main()
+        if opt == 'Master':
+            plot_treemap_master.main()
 
-with tab_best_match:
-    plot_best_match.main()
+    with tab_match:
+        plot_all_match.main()
 
-with tab_per_class:
-    plot_per_class_match.main()
+    with tab_best_match:
+        plot_best_match.main()
+
+    with tab_per_class:
+        plot_per_class_match.main({})
+
+    with tab_search:
+        plot_search.main()
